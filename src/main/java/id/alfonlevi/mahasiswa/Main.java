432d6391e0ce51df152816aba5d278674be6d7f2
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-05-10 21:12:59 +0700

    init

---
+++ src/main/java/id/alfonlevi/mahasiswa/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.alfonlevi.mahasiswa;
+
+/**
+ *
+ * @author levir
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/id/alfonlevi/mahasiswa/data/MahasiswaRepository.java
@@ -0,0 +1,5 @@
+package id.alfonlevi.mahasiswa.data;
+
+public interface MahasiswaRepository {
+    
+}
