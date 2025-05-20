0955f2b45f670cd8f13a49eb9a3b7d407acb36fd
AlfonsusSitanggang <alfonsussitanggang68@gmail.com>
2025-05-20 12:40:07 +0700

    nambah beberapa view

--- src/main/java/id/alfonlevi/mahasiswa/data/model/Dosen.java
+++ src/main/java/id/alfonlevi/mahasiswa/data/model/Dosen.java
@@ -4,12 +4,19 @@ public class Dosen extends Akun {
     private String mNip;
     private String mNama;
 
-    public Dosen(String username, String password, Role role, String nip, String nama) {
-        super(username, password, role);
+    public Dosen(String username, String password, String nip, String nama) {
+        super(username, password, Role.DOSEN);
         mNip = nip;
         mNama = nama;
     }
 
+/*************  ✨ Windsurf Command ⭐  *************/
+    /**
+     * Mendapatkan NIP dosen.
+     * 
+     * @return NIP dosen
+     */
+/*******  9e7976b1-5187-4642-9d01-5c907d4b1b10  *******/
     public String getNip() {
         return mNip;
     }
